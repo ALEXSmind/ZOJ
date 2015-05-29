51f0d98e844403287b6d57d31d88286dab42a0bc
ALEXSmind <alex.zxzy@gmail.com>
2015-05-29 11:18:20 +0800

    1034

---
+++ 1034.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int SNum,RNum;
+    bool CanRealized;
+    cin >> SNum;
+    for (int i =  0; i < SNum; i++)
+    {
+		cout << "Scenario #" <<i+1<<":"<<endl;
+		cin >> RNum;
+		for (int j = 0; j < RNum; j++)
+		{
+			CanRealized = false;
+			
+		}
+
+		cout << "Scenario #" <<i+1<<":"<<endl;
+
+	}
+}
